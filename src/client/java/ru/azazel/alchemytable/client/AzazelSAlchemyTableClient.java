d4fa9f7d3710a5f1ee89e49d826cbd6447a21c1f
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-07-13 11:24:48 +0300

    Add files via upload

---
+++ src/client/java/ru/azazel/alchemytable/client/AzazelSAlchemyTableClient.java
@@ -0,0 +1,10 @@
+package ru.azazel.alchemytable.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AzazelSAlchemyTableClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
