ca0018627cb4b34b6b8bcc9d23f30592b4db09fb
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-07-13 12:04:05 +0300

    Update AzazelSAlchemyTable.java

--- src/main/java/ru/azazel/alchemytable/AzazelSAlchemyTable.java
+++ src/main/java/ru/azazel/alchemytable/AzazelSAlchemyTable.java
@@ -6,6 +6,7 @@ import net.minecraft.resources.ResourceLocation;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.azazel.alchemytable.block.ModBlocks;
 
 public class AzazelSAlchemyTable implements ModInitializer {
 	public static final String MOD_ID = "azazels-alchemy-table";
@@ -20,6 +21,7 @@ public class AzazelSAlchemyTable implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		ModBlocks.registerModBlocks();
 
 		LOGGER.info("Hello Fabric world!");
 	}
