5172628a08af94c8606168134a94a9c049e7fb65
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-07-17 12:08:14 +0300

    Update AzazelSAlchemyTableClient.java

--- src/client/java/ru/azazel/alchemytable/client/AzazelSAlchemyTableClient.java
+++ src/client/java/ru/azazel/alchemytable/client/AzazelSAlchemyTableClient.java
@@ -1,16 +1,18 @@
 package ru.azazel.alchemytable.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.RenderType;
 import ru.azazel.alchemytable.block.ModBlocks;
-import net.minecraft.client.render.RenderLayer;
-import net.fabricmc.api.blockrenderlayer.v1.BlockRenderLayerMap;
 
 public class AzazelSAlchemyTableClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		BlockRenderLayerMap.INSTANCE.putBlock(
-			ModBlocks.ALCHEMY_TABLE,
-			RenderLayer.getCutout()
-		);
-	}
+
+    @Override
+    public void onInitializeClient() {
+        // Настраиваем отображение прозрачных участков модели.
+        BlockRenderLayerMap.INSTANCE.putBlock(
+                ModBlocks.ALCHEMY_TABLE,
+                RenderType.cutout()
+        );
+    }
 }
