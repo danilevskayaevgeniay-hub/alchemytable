6c4ef50ec7bbe7655741a6670bf38942dea7471e
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-07-17 11:25:04 +0300

    Update AzazelSAlchemyTableClient.java

--- src/client/java/ru/azazel/alchemytable/client/AzazelSAlchemyTableClient.java
+++ src/client/java/ru/azazel/alchemytable/client/AzazelSAlchemyTableClient.java
@@ -1,10 +1,15 @@
 package ru.azazel.alchemytable.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import ru.azazel.alchemytable.block.ModBlocks;
+import net.minecraft.client.render.RenderLayer;
+import net.fabricmc.api.blockrenderlayer.v1.BlockRenderLayerMap;
 
 public class AzazelSAlchemyTableClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		BlockRenderLayerMap.INSTANCE.putBlock(
+			ModBlocks.
+		)
 	}
 }
