c5d5dbee43e501331e7818b95144a2134fc4e4f3
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-07-21 11:53:28 +0300

    Update AlchemyTableBlock.java

--- src/main/java/ru/azazel/alchemytable/block/AlchemyTableBlock.java
+++ src/main/java/ru/azazel/alchemytable/block/AlchemyTableBlock.java
@@ -1,7 +1,9 @@
 package ru.azazel.alchemytable.block;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
@@ -9,17 +11,30 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class AlchemyTableBlock extends Block {
 
-    // Свойство хранит одно из четырёх горизонтальных направлений.
     public static final DirectionProperty FACING =
             BlockStateProperties.HORIZONTAL_FACING;
+
+    // Стол занимает две клетки и имеет высоту 25 пикселей.
+    private static final VoxelShape SHAPE_NORTH =
+            Block.box(-16, 0, 0, 16, 25, 16);
+
+    private static final VoxelShape SHAPE_EAST =
+            Block.box(0, 0, -16, 16, 25, 16);
+
+    private static final VoxelShape SHAPE_SOUTH =
+            Block.box(0, 0, 0, 32, 25, 16);
+
+    private static final VoxelShape SHAPE_WEST =
+            Block.box(0, 0, 0, 16, 25, 32);
 
     public AlchemyTableBlock(Properties properties) {
         super(properties);
 
-        // Направление блока по умолчанию.
         this.registerDefaultState(
                 this.stateDefinition.any()
                         .setValue(FACING, Direction.NORTH)
@@ -28,7 +43,6 @@ public class AlchemyTableBlock extends Block {
 
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        // При установке лицевая сторона стола поворачивается к игроку.
         return this.defaultBlockState()
                 .setValue(
                         FACING,
@@ -40,12 +54,44 @@ public class AlchemyTableBlock extends Block {
     protected void createBlockStateDefinition(
             StateDefinition.Builder<Block, BlockState> builder
     ) {
-        // Добавляем свойство направления в состояния блока.
         builder.add(FACING);
     }
 
+    private VoxelShape getTableShape(BlockState state) {
+        return switch (state.getValue(FACING)) {
+            case NORTH -> SHAPE_NORTH;
+            case EAST -> SHAPE_EAST;
+            case SOUTH -> SHAPE_SOUTH;
+            case WEST -> SHAPE_WEST;
+            default -> SHAPE_NORTH;
+        };
+    }
+
     @Override
-    public BlockState rotate(BlockState state, Rotation rotation) {
+    protected VoxelShape getShape(
+            BlockState state,
+            BlockGetter level,
+            BlockPos pos,
+            CollisionContext context
+    ) {
+        return getTableShape(state);
+    }
+
+    @Override
+    protected VoxelShape getCollisionShape(
+            BlockState state,
+            BlockGetter level,
+            BlockPos pos,
+            CollisionContext context
+    ) {
+        return getTableShape(state);
+    }
+
+    @Override
+    protected BlockState rotate(
+            BlockState state,
+            Rotation rotation
+    ) {
         return state.setValue(
                 FACING,
                 rotation.rotate(state.getValue(FACING))
@@ -53,10 +99,12 @@ public class AlchemyTableBlock extends Block {
     }
 
     @Override
-    public BlockState mirror(BlockState state, Mirror mirror) {
+    protected BlockState mirror(
+            BlockState state,
+            Mirror mirror
+    ) {
         return state.rotate(
                 mirror.getRotation(state.getValue(FACING))
         );
     }
 }
-
