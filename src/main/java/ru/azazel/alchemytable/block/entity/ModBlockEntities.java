885bcf30f9dc63b1d86ec84b30ef6ff549198981
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-07-24 12:46:50 +0300

    Update ModBlockEntities.java

--- src/main/java/ru/azazel/alchemytable/block/entity/ModBlockEntities.java
+++ src/main/java/ru/azazel/alchemytable/block/entity/ModBlockEntities.java
@@ -20,7 +20,8 @@ public class ModBlockEntities {
                     ),
 
                     FabricBlockEntityTypeBuilder.create(
-                           
+                        AlchemyTableBlockEntity::new,
+                        ModBlocks.
                     ).build()
             );
 
