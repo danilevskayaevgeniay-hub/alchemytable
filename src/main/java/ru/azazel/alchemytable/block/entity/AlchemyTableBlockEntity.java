a36fd2d46077c2e81bcb6296a23b535d50b98ee5
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-07-24 12:35:16 +0300

    Update AlchemyTableBlockEntity.java

--- src/main/java/ru/azazel/alchemytable/block/entity/AlchemyTableBlockEntity.java
+++ src/main/java/ru/azazel/alchemytable/block/entity/AlchemyTableBlockEntity.java
@@ -1,1 +1,21 @@
+package ru.azazel.alchemytable.block.entity;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class AlchemyTableBlockEntity extends BlockEntity {
+
+    public AlchemyTableBlockEntity(
+            BlockPos pos,
+            BlockState state
+    ) {
+        super(
+                ModBlockEntities.ALCHEMY_TABLE_BLOCK_ENTITY,
+                pos,
+                state
+        );
+    }
+}
+
 
