7760857da7c95f76a6f08cede514eada01067212
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-08-03 11:42:26 +0300

    Update AlchemyTableBlock.java

--- src/main/java/ru/azazel/alchemytable/block/AlchemyTableBlock.java
+++ src/main/java/ru/azazel/alchemytable/block/AlchemyTableBlock.java
@@ -20,6 +20,9 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.item.ItemStack;
 
 public class AlchemyTableBlock extends Block implements EntityBlock{
 
